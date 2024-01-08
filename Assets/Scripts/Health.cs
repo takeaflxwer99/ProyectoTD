9574d14d48d38606b672f7e989555811968c2230
emmahermon <151826474+emmahermon@users.noreply.github.com>
2024-01-08 16:15:54 +0100

    Currency/Health
    jiji

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [Header("Atributes")]
+    [SerializeField] private int hitPoints = 2;
+    [SerializeField] private int currencyWorth = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private bool isDestroyed = false;
+    public void TakeDamage (int dmg)
+    {
+        hitPoints -= dmg;
+        if (hitPoints <= 0 && !isDestroyed)
+        {
+            EnemyGroupManager.onEnemyDestroy.Invoke();
+            LevelManager.main.IncreaseCurrency(currencyWorth);
+            isDestroyed = true;
+            Destroy(gameObject);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{public static LevelManager main;
+    public Transform startPoint;
+    public Transform[] path;
+    public int currency;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        main = this;
+    }
+    void Start()
+    {
+        currency = 100;
+        
+    }
+    public void IncreaseCurrency(int amount)
+    {
+        currency += amount;
+    }
+    public bool SpendCurrency(int amount)
+    {
+        if (amount <= currency) {
+            currency -= amount;
+            return true;
+        }
+
+
+        else { Debug.Log("No tienes suficientes monedas");
+            return false;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
