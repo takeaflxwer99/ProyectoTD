c9044aac6afdea8eb82ccb1a4f69bbad903d0e2c
takeaflxwer99 <anghelteo0@gmail.com>
2024-01-09 10:03:24 +0100

    super mega menu hiper guay wow
    (no se puede cerrar aun jaja)

--- Assets/Menu.cs
+++ Assets/Menu.cs
@@ -7,14 +7,18 @@ public class Menu : MonoBehaviour
 {
    [Header("References")]
     [SerializeField] TextMeshProUGUI currencyUI;
+    [SerializeField] Animator anim;
+
+    private bool isMenuOpen = true;
+
+    public void ToggleMenu()
+    {
+        isMenuOpen = !isMenuOpen;
+        anim.SetBool("MenuOpen", isMenuOpen);
+    }
 
     private void OnGUI()
     {
         currencyUI.text = LevelManager.main.currency.ToString();
-    }
-
-    public void SetSelected()
-    {
-
     }
 }
