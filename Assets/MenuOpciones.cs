d697b3c8063d5bb9bcb902d11d85539c01fe80ff
emmahermon <151826474+emmahermon@users.noreply.github.com>
2024-01-18 21:56:58 +0100

    Menu opciones fin

--- Assets/MenuOpciones.cs
+++ Assets/MenuOpciones.cs
@@ -2,12 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MenuOpciones : MonoBehaviour
 {
-   public void SetVolume (float volume)
+    public Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+   
+      
+    public void SetVolume (float volume)
     { }
 
     public void SetQuality (int qualityIndex)
     { QualitySettings.SetQualityLevel(qualityIndex); }
+
+    public void SetFullscreen(bool isFullscreen)
+    { Screen.fullScreen = isFullscreen; }
+
+
+
 }
